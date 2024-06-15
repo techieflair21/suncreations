04c0f2dc08a329b209694fc7741f080a0fdad2e3
kuldeepsharma22 <50577375+kuldeepsharma22@users.noreply.github.com>
2024-06-15 16:37:18 +0530

    changes in index.js, index.css, added image

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -22,6 +22,8 @@ import arcylic1 from "../images/A1.jpeg"
 import arcylic2 from "../images/A2.jpeg"
 import arcylic3 from "../images/A3.jpeg"
 
+import formImg from "../images/acrylic.webp"
+
 const Home = () => {
   const AutoplaySlider = withAutoplay(AwesomeSlider);
     return (
@@ -59,6 +61,33 @@ const Home = () => {
             <img src={arcylic2} alt="a2" />
             <img src={arcylic3} alt="a3" />
         </div>
+      </section>
+
+      <section className="arcylic_form">
+        <img src={formImg} />
+        <form>
+          <div className="form-box1 form_comman_style">
+            <label>Name <span>*</span></label>
+            <input type="text" name="name" />
+          </div>
+          <div className="form-box2 form_comman_style">
+            <label>Email <span>*</span></label>
+            <input type="email" name="email" />
+          </div>
+          <div className="form-box2 form_comman_style">
+            <label>Contact <span>*</span></label>
+            <input type="number" name="number" />
+          </div>
+          <div className="form-box2 form_comman_style">
+            <label>Address <span>*</span></label>
+            <input type="text" name="address" />
+          </div>
+          <div className="form-box2 form_comman_style">
+            <label>Message <span>*</span></label>
+            <textarea name="textarea" />
+          </div>
+          <input type="submit" name="Send Message" />
+        </form>
       </section>
       
       <section id="tours" className="home-quick-view">
