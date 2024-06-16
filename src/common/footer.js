6226279a9f430d3af9c790e769dd6d9d84072da0
kuldeepsharma22 <50577375+kuldeepsharma22@users.noreply.github.com>
2024-06-16 08:06:36 +0530

    index.css, footer.js updated

--- src/common/footer.js
+++ src/common/footer.js
@@ -12,10 +12,10 @@ import imgBox6 from  "../assets/images/Project-6.jpeg"
 const Footer = () => {
     return (
         <>
-{/* <footer className="footer-distributed">
+<footer className="footer-distributed">
     <div className="footer_col1 footer_comman_style">
         <img src ={logo} alt="footer-logo" height="60px" />
-        <p>Welcome to Sun Creation, where you can find a large selection of high-quality home furnishings and accessories. Sun Creation, company founded with a desire  for designing elegant and practical environments, pulls together an amazing selection that enhances  the beauty of both your indoor and outdoor settings.</p>
+        <p>Welcome to Sun Creation, where you can find a large selection of high-quality home furnishings and accessories.</p>
     </div>
     <div className="footer_col2 footer_comman_style">
         <h4>Address</h4>
@@ -30,7 +30,7 @@ const Footer = () => {
     </div>
     <div className="footer_col3 footer_comman_style">
         <h4>Location</h4>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3499.6147512395814!2d77.12803931440894!3d28.701168687723474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d03deecbf10bf%3A0x91f954db4fd2c105!2sSignex%20Print%20Media!5e0!3m2!1sen!2sin!4v1588333215895!5m2!1sen!2sin" width="100%" height="" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3499.6147512395814!2d77.12803931440894!3d28.701168687723474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d03deecbf10bf%3A0x91f954db4fd2c105!2sSignex%20Print%20Media!5e0!3m2!1sen!2sin!4v1588333215895!5m2!1sen!2sin" width="100%" height="" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
     </div>
     <div className="footer_col3 footer_comman_style">
         <h4>Projects</h4>
@@ -43,7 +43,7 @@ const Footer = () => {
             <img src ={imgBox6} alt="project 6" />
         </div>
     </div>
-</footer> */}
+</footer>
 <div className="copyright"> <p className="footer-company-name">Sun Creation © 2023</p></div>
 </>
     )
