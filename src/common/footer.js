70651ac04d09a5e5935d3f44acd327fb0defc596
kuldeepsharma22 <50577375+kuldeepsharma22@users.noreply.github.com>
2024-06-29 16:46:55 +0530

    footer.js, index.css
    footer.js, index.css

--- src/common/footer.js
+++ src/common/footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import logo from  "../assets/images/footer-logo.png"
 import "./assets/index.css"
@@ -14,26 +15,34 @@ const Footer = () => {
         <>
 <footer className="footer-distributed">
     <div className="footer_col1 footer_comman_style">
-        <img src ={logo} alt="footer-logo" height="60px" />
-        <p>Welcome to Sun Creation, where you can find a large selection of high-quality home furnishings and accessories.</p>
+        <h4>QUICK LINKS</h4>
+        <ul>
+            <li><a href="/">Home<i className="material-icons"></i></a></li>
+            <li><Link to="/about-us">About Us<i className="material-icons"></i></Link></li>
+            <li><Link to="/our-service">Our Services<i className="material-icons"></i></Link></li>
+            <li><Link to="/our-portfolio">Our Portfolio<i className="material-icons"></i></Link></li>
+            <li><Link to="/contact-us">Contact Us<i className="material-icons"></i></Link></li>
+            <li><Link to="/enquiry">Enquiry<i className="material-icons"></i></Link></li>
+        </ul>
     </div>
     <div className="footer_col2 footer_comman_style">
-        <h4>Address</h4>
-        <p>SIGNEX PRINT MEDIA <br />
-        Plot No. 182, Ground Floor, <br />
-        Rama Market, Pitampura, <br />
-        New Delhi-110034 <br />
-
-        Telephone :  <a href="tel:01142696157">01142696157</a> <br />
-        Working Day :  Monday - Saturday <br />
-        Timings :  10:00 AM - 7:30 PM</p>
+        <h4>CONTACT WITH US</h4>
+        <p>Phone: <a href="tel:+918840966752">+91 8840966752</a></p>
+        <p>Email: <a href="mailto:info.suncreations@gmail.com">info.suncreations@gmail.com</a></p>
+        <p>Working Day :  Monday - Saturday <br /> Timings :  10:00 AM - 7:30 PM</p>
     </div>
     <div className="footer_col3 footer_comman_style">
-        <h4>Location</h4>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3499.6147512395814!2d77.12803931440894!3d28.701168687723474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d03deecbf10bf%3A0x91f954db4fd2c105!2sSignex%20Print%20Media!5e0!3m2!1sen!2sin!4v1588333215895!5m2!1sen!2sin" width="100%" height="" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        <h4>FOLLOW US</h4>
+        <ul>
+            <li><a href="https://www.instagram.com/SunCreationforyou/" target="_blank"><i className="fa fa-instagram"></i></a></li>
+            <li><a href="https://www.facebook.com/people/Pink-Pages/61550884605810/" target="_blank"><i className="fa fa-facebook"></i></a></li>
+            <li><a href="https://www.youtube.com/@SunCreations" target="_blank"><i className="fa fa-youtube"></i></a></li>
+        </ul>
     </div>
-    <div className="footer_col3 footer_comman_style">
-        <h4>Projects</h4>
+    <div className="footer_col4 footer_comman_style">
+        <img src ={logo} alt="footer-logo" height="60px" />
+        <p>Welcome to Sun Creation, where you can find a large selection of high-quality home furnishings and accessories.</p>
+        {/* <h4>Projects</h4>
         <div className="footer_img_box">
             <img src ={imgBox1} alt="project 1" />
             <img src ={imgBox2} alt="project 2" />
@@ -41,10 +50,10 @@ const Footer = () => {
             <img src ={imgBox4} alt="project 4" />
             <img src ={imgBox5} alt="project 5" />
             <img src ={imgBox6} alt="project 6" />
-        </div>
+        </div> */}
     </div>
 </footer>
-<div className="copyright"> <p className="footer-company-name">Sun Creation © 2023</p></div>
+<div className="copyright"> <p className="footer-company-name">All Rights Reserved Sun Creation © 2024</p></div>
 </>
     )
     
